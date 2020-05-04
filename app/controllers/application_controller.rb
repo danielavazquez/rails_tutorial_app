b891f26ea5fd39d4f57044c585d76ecfc5c3534f
Ubuntu <ubuntu@ip-172-31-44-175.us-east-2.compute.internal>
2020-05-04 22:40:04 +0000

    Minor updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-<<<<<<< HEAD
-  
- def hello
-    render html: "hello, world!"
- end
-end
-=======
-end
->>>>>>> basic-login
